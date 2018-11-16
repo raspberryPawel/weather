5afbe4765afefc34f74ad274c94920d74fb19494
Paweł Malina <pawel.malina1999@gmail.com>
2018-11-16 22:37:14 +0100

    first commit

---
+++ php/getLocation.php
@@ -0,0 +1,18 @@
+<?php
+try {
+    $apikey = "JLlJBvgHU5dahEX2Atbq4fB4ePqH8eYi";
+    error_reporting(E_ERROR | E_PARSE);
+    $toSearch = $_POST['search'];
+    $ch = curl_init();
+        // CURLOPT_URL, CURLOPT_RETURNTRANSFER, CURLOPT_POST, CURLOPT_POSTFIELDS
+    curl_setopt($ch, CURLOPT_URL, "http://dataservice.accuweather.com/locations/v1/".$toSearch."?apikey=".$apikey."&language=pl-pl&details=true");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_GET, true);
+
+    $server_output = curl_exec($ch);
+    curl_close($ch);
+    echo (json_encode($server_output));
+        // echo(json_encode($html));
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
---
+++ php/getWeather.php
@@ -0,0 +1,16 @@
+<?php
+try {
+    $apikey = "JLlJBvgHU5dahEX2Atbq4fB4ePqH8eYi";
+    error_reporting(E_ERROR | E_PARSE);
+    $toSearch = $_POST['search'];
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "http://dataservice.accuweather.com/forecasts/v1/daily/5day/".$toSearch."?apikey=".$apikey."&language=pl-pl&details=true");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_GET, true);
+
+    $server_output = curl_exec($ch);
+    curl_close($ch);
+    echo (json_encode($server_output));
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
