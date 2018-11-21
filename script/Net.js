91409c7c4d32727c8f72bdaf449fba7c8289daa1
Paweł Malina <pawel.malina1999@gmail.com>
2018-11-22 00:31:18 +0100

    start admin page | login register | cookie

--- script/Net.js
+++ script/Net.js
@@ -17,23 +17,95 @@ $(document).ready(function () {
                         console.log(obj);
                         if (obj == "mistake")
                             $("#info").html("Na podany email utworzono już konto");
-                        else if (obj == "success")
+                        else if (obj == "success") {
                             $("#info").html("Gratulacje, konto zostało założone");
+                            setTimeout(function () {
+                                $("#registerForm").css("transition", "2s").css("top", "-1000%");
+                            }, 1500);
+                        }
+
+
                     },
                     error: function (xhr, status, error) {
                     },
                 });
-                console.log("identyczne");
             }
             else {
                 $("#info").html("Podane hasła są różne");
-                console.log("taki chuj nie identyczne");
             }
+        }
+        else {
+            $("#info").html("Uzupełnij wymagane pola");
+        }
+    });
+
+    $("#loginSubmit").on("click", function () {
+        console.log($("#loginSubmit"))
+        if ($("#inputEmail")[0].value.length > 0 && $("#inputPassword")[0].value.length > 0) {
+            $("#loginInfo").html("");
+            var e = $("#inputEmail")[0].value;
+            var pass = $("#inputPassword")[0].value;
+            console.log(e, pass);
+            $.ajax({
+                url: "php/Login.php",
+                data: { email: e, password: pass },
+                type: "POST",
+                success: function (data) {
+                    var obj = data;
+                    console.log(obj);
+                    if (obj == "mistake")
+                        $("#loginInfo").html("Podany email nie figuruje w naszej bazie");
+                    else if (obj == "valid") {
+                        $("#loginInfo").html("Zalogowano");
+                        setTimeout(function () {
+                            $("#login").css("transition", "2s").css("top", "-1000%");
+                        }, 1500);
+                    }
+                    else if (obj == "invalidPassword")
+                        $("#loginInfo").html("Hasło niepoprawne");
+                },
+                error: function (xhr, status, error) {
+                },
+            });
         }
         else {
             $("#info").html("Uzupełnij wymagane pola");
             console.log("taki chuj nie identyczne");
         }
-
     });
 });
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function setCookie(key, value, expireDays, expireHours, expireMinutes, expireSeconds) {
+    var expireDate = new Date();
+    if (expireDays) {
+        expireDate.setDate(expireDate.getDate() + expireDays);
+    }
+    if (expireHours) {
+        expireDate.setHours(expireDate.getHours() + expireHours);
+    }
+    if (expireMinutes) {
+        expireDate.setMinutes(expireDate.getMinutes() + expireMinutes);
+    }
+    if (expireSeconds) {
+        expireDate.setSeconds(expireDate.getSeconds() + expireSeconds);
+    }
+    document.cookie = key +"="+ escape(value) +
+        ";domain="+ window.location.hostname +
+        ";path=/"+
+        ";expires="+expireDate.toUTCString();
+}
+
+function deleteCookie(name) {
+    setCookie(name, "", null , null , null, 1);
+}
--- script/UI.js
+++ script/UI.js
@@ -1,25 +1,53 @@
 $(document).ready(function () {
+    //tolltip
     $('[data-toggle="tooltip"]').tooltip();
     // wysuwane menu
     var click = 0;
     $("#menu-button").on("click", function () {
         click = show_Menu(click);
+        var x = getCookie('userName');
+        if (x == null) {
+            $("#online").css("display", "none");
+            $("#offline").css("display", "block");
+        }
+        else {
+            $("#offline").css("display", "none");
+            $("#online").css("display", "block");
+        }
+        console.log("cookie: ==> ", x);
     });
-
+    //hide login panel
     $("#loginCancel").on("click", function () {
-        $("#login").css("transition", "1s").css("top", "-1000%");
+        $("#login").css("transition", "2s").css("top", "-1000%");
     });
+    //hide register panel
     $("#registerCancel").on("click", function () {
-        $("#registerForm").css("transition", "1s").css("top", "-1000%");
+        $("#registerForm").css("transition", "2s").css("top", "-1000%");
     });
-
+    //show login panel
     $("#sigIn").on("click", function () {
         $("#login").css("transition", "1s").css("top", "0%");
         click = show_Menu(click);
     });
+    //show register panel
     $("#register").on("click", function () {
         $("#registerForm").css("transition", "1s").css("top", "0%");
         click = show_Menu(click);
+    });
+
+    //logout
+    $("#logout").on("click", function () {
+        deleteCookie('userName');
+        $("#login").css("transition", "2s").css("top", "-1000%");
+        click = show_Menu(click);
+    });
+    //admin page sidebar 
+    $("#dismiss").on("click", function () {
+        $("#adminPage nav").animate({ left: "-350px" }, 500);
+    });
+
+    $("#show").on("click", function () {
+        $("#adminPage nav").animate({ left: "0px" }, 500);
     });
 });
 
